0b71df315c92e3f1f274ce5bdec048922205af68
PavlaK <paja.koudy@gmail.com>
2015-04-21 10:22:14 +0200

    Update index.js

--- snippets/Basic-Authentication-HttpClient/controlles/index.js
+++ snippets/Basic-Authentication-HttpClient/controlles/index.js
@@ -1,1 +1,10 @@
 
+$.index.open();
+  //save login information to local storage from the form in login.js
+function saveValues(e){
+Ti.App.Properties.setString('username', $.username.value);
+Ti.App.Properties.setString('password', $.password.value);
+
+  // open login.xml window
+	Alloy.createController('login').getView().open();
+};
